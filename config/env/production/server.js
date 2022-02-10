352c979583985d9976c2dc1d19ec744023bbda1a
Matt Haynes <Matthew.haynes@joipolloi.com>
2022-02-10 16:19:50 +0000

    Add key nmes to server.js

--- config/env/production/server.js
+++ config/env/production/server.js
@@ -1,3 +1,7 @@
 module.exports = ({ env }) => ({
     url: env('MY_HEROKU_URL'),
+    proxy: true,
+    app: {
+        keys: env.array("APP_KEYS", ["DATABASE_URL", "HEROKU_POSTGRESQL_MAROON_URL"]),
+    },
 });
